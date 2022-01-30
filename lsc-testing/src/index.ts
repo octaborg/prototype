cea157eeb6b8bd01ee0749b46a1eae19b18b530c
Vim Wickramasinghe <vim@klar.mx>
2022-01-30 20:16:04 +0100

    LSC testing contract init

---
+++ lsc-testing/src/index.ts
@@ -0,0 +1,90 @@
+import {
+  Field,
+  PublicKey,
+  SmartContract,
+  state,
+  State,
+  method,
+  UInt64,
+  Int64,
+  Mina,
+  isReady,
+  PrivateKey,
+  Party,
+  shutdown
+} from 'snarkyjs';
+import { AccountStatement, RequiredProof, RequiredProofs, RequiredProofType, TransactionalProof } from './octa.js';
+
+/**
+ * Loan smart contract interface
+ */
+export default class LSC extends SmartContract {
+  @state(Field) interestRate = State<Field>();
+  @state(Field) termInDays = State<Field>();
+  @state(RequiredProofs) requiredProofs = State<RequiredProofs>();
+
+  // Terms of the loan are injected at deployment. Called by the lender.
+  deploy(
+    loanAmount: UInt64,
+    interestRate: Field,
+    termInDays: Field,
+    requiredProofs: RequiredProofs
+  ) {
+    super.deploy();
+    this.balance.addInPlace(loanAmount);
+    this.interestRate.set(interestRate);
+    this.termInDays.set(termInDays);
+    this.requiredProofs.set(requiredProofs);
+  }
+
+  // Request a loan with required proofs. Called by the borrower
+  @method 
+  async requestLoan(amount: UInt64, accountStatement: AccountStatement) {
+    new TransactionalProof(accountStatement, await this.requiredProofs.get()).validate();
+
+  }
+
+  // Approve the loan for the given address. Called by the lender.
+  // This would be useful when lenders optimize on the type of borrowers
+  // based on the demand and other factors.
+  @method 
+  async approve(address: PublicKey) {
+  }
+
+  // Accept the loan for the calling address. Called by the borrower.
+  @method 
+  async accept() {
+  }
+
+}
+
+// setup
+async function deploy() {
+  await isReady;
+  const Local = Mina.LocalBlockchain();
+  Mina.setActiveInstance(Local);
+  const lender = Local.testAccounts[0].privateKey;
+  const account2 = Local.testAccounts[1].privateKey;
+
+  const snappPrivkey = PrivateKey.random();
+  let snappAddress = snappPrivkey.toPublicKey();
+
+  console.log('Deploying Snapp...');
+  let tx = Mina.transaction(lender, async () => {
+    const initialBalance = UInt64.fromNumber(1000000);
+    const p = await Party.createSigned(account2);
+    p.balance.subInPlace(initialBalance);
+    let snapp = new LSC(snappAddress);
+    snapp.deploy(initialBalance, new Field(1), new Field(365), 
+    new RequiredProofs([new RequiredProof(
+      RequiredProofType.avgMonthlyIncomeProof(), 
+      new Int64(new Field(100000)), 
+      new Int64(new Field(3000)))]));
+  });
+  await tx.send().wait();
+}
+
+deploy();
+// console.log('Is the sudoku solved?', (await getSnappState()).isSolved);
+
+shutdown();
---
+++ lsc-testing/src/octa.ts
@@ -0,0 +1,194 @@
+import {
+  Field,
+  PublicKey,
+  UInt64,
+  CircuitValue,
+  prop,
+  Bool,
+  Int64,
+  Signature,
+  Circuit,
+} from 'snarkyjs';
+
+// This is an enum written as a class, is there a way to just define an enum in a
+// Snarky compatible way?
+export class TransactionType extends CircuitValue {
+  @prop purchase: Bool;
+  @prop deposit: Bool;
+  @prop transferIn: Bool;
+  @prop transaferOut: Bool;
+
+
+  constructor(
+    purchase: Bool,
+    deposit: Bool,
+    transferIn: Bool,
+    transaferOut: Bool
+  ) {
+    super();
+    this.purchase = purchase;
+    this.deposit = deposit;
+    this.transferIn = transferIn;
+    this.transaferOut = transaferOut;
+  }
+
+}
+
+export class Transaction extends CircuitValue {
+  @prop id: Field;
+  @prop amount: Int64;
+  @prop transactionType: TransactionType;
+  @prop timestamp: Int64;
+
+
+  constructor(
+    id: Field,
+    amount: Int64,
+    transactionType: TransactionType,
+    timestamp: Int64
+  ) {
+    super();
+    this.id = id
+    this.amount = amount
+    this.transactionType = transactionType
+    this.timestamp = timestamp
+  }
+
+}
+
+export class AccountStatement {
+  id: Field;
+  balance: UInt64;
+  timestamp: Int64;
+  fromTimestamp: Int64;
+  toTimestamp: Int64;
+  transactions: Transaction[];
+  authorityPublicKey: PublicKey;
+  signature: Signature;
+
+  constructor(
+    id: Field,
+    balance: UInt64,
+    timestamp: Int64,
+    from: Int64,
+    to: Int64,
+    transactions: Array<Transaction>,
+    authorityPublicKey: PublicKey,
+    signature: Signature
+  ) {
+    this.id = id;
+    this.balance = balance;
+    this.timestamp = timestamp;
+    this.fromTimestamp = from;
+    this.toTimestamp = to;
+    this.transactions = transactions;
+    this.authorityPublicKey = authorityPublicKey;
+    this.signature = signature;
+  }
+
+  verifySignature() {
+    // TODO
+  }
+
+}
+
+export class TransactionalProof {
+  account: AccountStatement;
+  requiredProofs: RequiredProofs;
+
+  constructor(account: AccountStatement, requiredProofs: RequiredProofs) {
+    this.account = account;
+    this.requiredProofs = requiredProofs;
+  }
+
+  validate() {
+    this.account.verifySignature();
+    // TODO prevent same proof from being calculated multiple times
+    let validated = new Bool(true);
+    for (let i = 0; i < this.requiredProofs.requiredProofs.length; i++) {
+      validated = validated.and(Circuit.if(
+        this.requiredProofs.requiredProofs[i].requiredProofType.equals(RequiredProofType.avgMonthlyBalanceProof()),
+        this.validateAvgMonthlyBalanceProof(this.requiredProofs.requiredProofs[i]),
+        new Bool(true)));
+
+      validated = validated.and(Circuit.if(
+        this.requiredProofs.requiredProofs[i].requiredProofType.equals(RequiredProofType.avgMonthlyIncomeProof()),
+        this.validateAvgMonthlyIncomeProof(this.requiredProofs.requiredProofs[i]),
+        new Bool(true)));
+    }
+  }
+
+  validateAvgMonthlyBalanceProof(requiredProof: RequiredProof): Bool {
+    let avgMonthlyBalance = new Int64(new Field(5000)); // TODO <- calculate this
+    return requiredProof.lowerBound.value.lte(avgMonthlyBalance.value)
+    .and(requiredProof.upperBound.value.gt(avgMonthlyBalance.value));
+  }
+
+  validateAvgMonthlyIncomeProof(requiredProof: RequiredProof): Bool {
+    let avgMonthlyIncome = new Int64(new Field(5000)); // TODO <- calculate this
+    return requiredProof.lowerBound.value.lte(avgMonthlyIncome.value)
+    .and(requiredProof.upperBound.value.gt(avgMonthlyIncome.value));
+  }
+
+}
+
+// This is an enum written as a class, is there a way to just define an enum in a
+// Snarky compatible way?
+export class RequiredProofType extends CircuitValue {
+  // Calculated based on the amount of incoming 
+  // transactions over the past 3 months
+  @prop avgMonthlyIncomeProof: Bool;
+
+  // Calculated based on the end of month balance 
+  // for the past 3 months
+  @prop avgMonthlyBalanceProof: Bool;
+
+  private constructor(
+    avgMonthlyIncomeProof: Bool,
+    avgMonthlyBalanceProof: Bool
+  ) {
+    super();
+    this.avgMonthlyBalanceProof = avgMonthlyBalanceProof;
+    this.avgMonthlyIncomeProof = avgMonthlyIncomeProof;
+  }
+
+  static avgMonthlyIncomeProof(): RequiredProofType {
+    return new RequiredProofType(new Bool(true), new Bool(false));
+  }
+
+  static avgMonthlyBalanceProof(): RequiredProofType {
+    return new RequiredProofType(new Bool(false), new Bool(true));
+  }
+}
+
+export class RequiredProof extends CircuitValue {
+  @prop requiredProofType: RequiredProofType;
+  @prop upperBound: Int64;
+  @prop lowerBound: Int64;
+
+  constructor(
+    requiredProofType: RequiredProofType,
+    upperBound: Int64,
+    lowerBound: Int64) {
+    super();
+    this.requiredProofType = requiredProofType;
+    this.upperBound = upperBound;
+    this.lowerBound = lowerBound;
+  }
+
+}
+
+const MAX_REQUIRED_PROOFS = 10;
+export class RequiredProofs extends CircuitValue {
+  @prop requiredProofs: Array<RequiredProof>;
+  
+
+  constructor(requiredProofs: Array<RequiredProof>) {
+    super();
+    this.requiredProofs = requiredProofs;
+  }
+  
+}
+//(RequiredProofs.prototype as any)._fields = [['value', Circuit.array(RequiredProof, MAX_REQUIRED_PROOFS)]];
+
+
