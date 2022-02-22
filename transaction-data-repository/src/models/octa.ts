006d5826d844d95480c8818996de529b4f3705a0
marekyggdrasil <marek.yggdrasil@gmail.com>
2022-02-22 21:30:44 +0800

    adapting to use the octa-types module

--- transaction-data-repository/src/models/octa.ts
+++ transaction-data-repository/src/models/octa.ts
@@ -1,87 +1,9 @@
-import { Bool, Signature, UInt64, PrivateKey, Int64, Field, isReady, shutdown, Circuit, JSONValue } from "snarkyjs";
-
 import {
-    AccountStatement,
-    AccountStatementSigned,
-    RequiredProofs,
-    Transaction,
-    TransactionalProof,
-    TransactionType,
-    RequiredProof,
-    RequiredProofType
-} from "../entity/octa";
-
-const getOCTATransactions = async () => {
-    const transactions: [Transaction] = [
-        new Transaction(
-            new Field(1),
-            new Int64(new Field(5000)), // TODO adjust timestamp to pass tests
-            new TransactionType(
-                new Bool(true),
-                new Bool(false),
-                new Bool(false),
-                new Bool(false)
-            ),
-            new Int64(new Field(0)))
-    ];
-    return transactions;
-};
-
-const getOCTATransaction = async (id: Number) => {
-    const transaction: Transaction = new Transaction(
-        new Field(1),
-        new Int64(new Field(5000)), // TODO adjust timestamp to pass tests
-        new TransactionType(
-            new Bool(true),
-            new Bool(false),
-            new Bool(false),
-            new Bool(false)
-        ),
-        new Int64(new Field(0)))
-    return transaction;
-};
-
-function castString(f: JSONValue | null): JSONValue {
-    if (f === null) {
-        console.log("throwing error");
-        throw Error();
-    }
-    return f;
-}
+    AccountStatement
+} from "octa-types";
 
 const getOCTAAccountStatement = async (id: Number) => {
-    const inp: JSONValue = (new Field(0)).toJSON();
-    console.log(typeof(inp));
-    try {
-        const v: Field | null = Field.fromJSON((new Field(0)).toJSON());
-    } catch (err) {
-        console.log(err);
-    }
-    console.log("lol");
-    /*
-    if (v === null) {
-        console.log('is null');
-    } else {
-        console.log('is not null');
-    }*/
-    const account: AccountStatement = new AccountStatement(
-        new Field(0),
-        new UInt64(new Field(10000)),
-        new Int64(new Field(100)), // timestamp
-        new Int64(new Field(100)),
-        new Int64(new Field(100)),
-        [new Transaction(
-            new Field(1),
-            new Int64(new Field(5000)), // TODO adjust timestamp to pass tests
-            new TransactionType(
-                new Bool(true),
-                new Bool(false),
-                new Bool(false),
-                new Bool(false)
-            ),
-            new Int64(new Field(0)))
-        ]);
-    return account;
+    return AccountStatement.dummy();
 };
 
-export default { getOCTATransactions, getOCTATransaction, getOCTAAccountStatement };
+export default { getOCTAAccountStatement };
