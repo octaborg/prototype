de992ca22a31741199c5bad05fa7859b857eb174
marekyggdrasil <marek.yggdrasil@gmail.com>
2022-02-28 22:06:44 +0800

    producing some dummy values

--- transaction-data-repository/src/models/octa.ts
+++ transaction-data-repository/src/models/octa.ts
@@ -1,9 +1,43 @@
 import {
+    TransactionType,
+    Transaction,
     AccountStatement
 } from "octa-types";
 
+
+import {
+    Field,
+    Bool,
+    Int64,
+    UInt64
+} from 'snarkyjs';
+
 const getOCTAAccountStatement = async (id: Number) => {
-    return AccountStatement.dummy();
+    const transactions: Transaction[] = [];
+    for (let j = 0; j < 100; ++j) {
+        transactions.push(
+            new Transaction(
+                new Field(1),
+                new Int64(new Field(5000)), // TODO adjust timestamp to pass tests
+                new TransactionType(
+                    new Bool(true),
+                    new Bool(false),
+                    new Bool(false),
+                    new Bool(false)
+                ),
+                new Int64(new Field(1))
+            )
+        );
+    }
+    const account: AccountStatement = new AccountStatement(
+        new Field(1),
+        new UInt64(new Field(10000)),
+        new Int64(new Field(100)), // timestamp
+        new Int64(new Field(100)),
+        new Int64(new Field(100)),
+        transactions
+    );
+    return account;
 };
 
 export default { getOCTAAccountStatement };
