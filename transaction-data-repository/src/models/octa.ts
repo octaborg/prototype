b6b963ad6e0961df15513e2faff6595bb5938151
marekyggdrasil <marek.yggdrasil@gmail.com>
2022-03-26 15:40:07 +0800

    infinite database of accounts

--- transaction-data-repository/src/models/octa.ts
+++ transaction-data-repository/src/models/octa.ts
@@ -2,6 +2,8 @@ import {
     generateDummyAccount
 } from "octa-types";
 
+import { XORShift } from 'random-seedable';
+
 
 import {
     Field,
@@ -11,19 +13,11 @@ import {
 } from 'snarkyjs';
 
 const getOCTAAccountStatement = async (id: number) => {
-    switch(id) {
-        case 0: return generateDummyAccount(id, 1000, 88, 100);
-        case 1: return generateDummyAccount(id, 1200, 125, 3000);
-        case 2: return generateDummyAccount(id, 900, 99, 50000);
-        case 3: return generateDummyAccount(id, 800, 25, 8500);
-        case 4: return generateDummyAccount(id, 5000, 300, 1200);
-        case 5: return generateDummyAccount(id, 6000, 320, 3400);
-        case 6: return generateDummyAccount(id, 4500, 400, 43200);
-        case 7: return generateDummyAccount(id, 3200, 350, 1000000);
-        case 8: return generateDummyAccount(id, 1800, 220, 8400);
-        case 9: return generateDummyAccount(id, 1800, 210, 20200);
-        default: return generateDummyAccount(id, 1000, 88, 100);
-    }
+    const random = new XORShift(id);
+    const income: number = random.randRange(990, 2800);
+    const daily_expense: number = random.randRange(60, 150);
+    const final_balance: number = random.randRange(7000, 25000);
+    return generateDummyAccount(id, income, daily_expense, final_balance);
 };
 
 export default { getOCTAAccountStatement };
